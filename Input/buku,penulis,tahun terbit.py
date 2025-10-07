c9d6a749f6cdeebc5451c1812bde435ac6170d90
annisa090812 <annisawahibbah9@gmail.com>
2025-10-07 07:00:18 +0700

    Add files via upload

---
+++ Input/buku,penulis,tahun terbit.py
@@ -0,0 +1,5 @@
+judul_buku = input("masukkan judul buku: ")
+nama_penulis = input("masukkan nama penulis: ")
+tahun_terbit = int(input("masukkan tahun terbit: "))
+
+print (f"Buku {judul_buku}, di tulis oleh {nama_penulis}, dan diterbitkan pada tahun {tahun_terbit}")
---
+++ Input/perkenalan.py
@@ -0,0 +1,5 @@
+nama_depan = input("masukkan nama depan: ")
+nama_belakang = input("masukkan nama belakang: ")
+umur = int(input("masukkan umur: "))
+
+print (f"Perkenalkan saya {nama_depan} {nama_belakang}, saya berusia {umur} tahun")
